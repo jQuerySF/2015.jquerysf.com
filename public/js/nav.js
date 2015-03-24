27073274e584cbe5f90720b33e51ba52f06b2bc8
Myles Borins <myles@famo.us>
2015-03-23 18:22:10 -0700

    Hide some things

--- public/js/nav.js
+++ public/js/nav.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       var $target = $(target);
 
       $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
+          'scrollTop': $target.offset().top - 50
       }, 900, 'swing', function () {
           window.location.hash = target;
       });
